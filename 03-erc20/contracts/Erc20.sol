6cdf581e7c5e7f5b8f9977c5f8d46303f0d31142
Cal Angela Mae A. <99713705+Angela-Mae-Cal@users.noreply.github.com>
2025-02-28 07:26:12 +0000

    calibear

--- 03-erc20/contracts/Erc20.sol
+++ 03-erc20/contracts/Erc20.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract NameYourToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("NameYourToken", "NYT") {
+    constructor(uint256 initialSupply) ERC20("CaliCoin", "CC") {
         // _mint(msg.sender, initialSupply);
     }
 
